823ea107e7dd1de815406e7f8058dd6de754c9db
Prathamesh-Mandiye-0423 <mandiyeprathamesh@gmail.com>
2026-02-04 13:13:57 +0530

    Updated with Backend URL

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL =  'https://line-segment-separator-1.onrender.com' || 'http://localhost:5000/api';
 
 export const computeRectangles = async (redSegments, blueSegments) => {
   try {
